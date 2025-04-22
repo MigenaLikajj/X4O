a98ebb13c23b745e5706c90ae0376c9f632ba475
migena likaj <migena.likaj@studentet.uni-pr.edu>
2025-04-22 21:40:24 +0200

    Add utility functions for best-move filtering and random generator splitting

--- src/AI.hs
+++ src/AI.hs
@@ -7,3 +7,19 @@ import Common (randomPick)
 class GamePosition a where
   moves :: a -> [a]
   static :: a -> Int
+
+-- We take maximum of the negative of the scores
+gatherMax :: [(Int, ((Maybe Int, Int), a))] -> (Int, ((Maybe Int, Int), a)) -> [(Int, ((Maybe Int, Int), a))]
+gatherMax [] x = x:[]
+gatherMax xs@((_, ((_, bscore), _)):_) x@(_, ((_, score), _)) =
+  if score == bscore
+  then x:xs
+  else if (score < bscore)
+       then x:[]
+       else xs
+
+genRandom :: (RandomGen g) => g -> Int -> [StdGen] -> ([StdGen], g)
+genRandom orig 0 gs = (gs, orig)
+genRandom orig x gs = let (seed, orig') = random orig
+                          g = mkStdGen seed
+                      in genRandom orig' (x - 1) (g:gs)
