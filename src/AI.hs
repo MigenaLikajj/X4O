cd8911d870a3a1445db8c24fc65f693d4cffe532
migena likaj <migena.likaj@studentet.uni-pr.edu>
2025-04-22 21:40:59 +0200

    Implement the negmax algorithm for AI move selection

--- src/AI.hs
+++ src/AI.hs
@@ -23,3 +23,17 @@ genRandom orig 0 gs = (gs, orig)
 genRandom orig x gs = let (seed, orig') = random orig
                           g = mkStdGen seed
                       in genRandom orig' (x - 1) (g:gs)
+
+negmax :: (GamePosition a, RandomGen g) => Int -> a -> g -> ((Maybe Int, Int), g)
+negmax depth b gen
+ | depth == 0     = ((Nothing, static b), gen)
+ | null (moves b) = ((Nothing, static b), gen)
+ | otherwise =
+     let posMoves      = moves b
+         (gens, gen')  = genRandom gen (length posMoves) []
+         results       = zipWith (\g mov -> negmax (depth - 1) mov g) gens (moves b)
+         zipped        = zip [0..] results
+         bests         = foldl gatherMax [] zipped
+         (best, gen'') = randomPick bests gen'
+         (moveIndex, ((_, score), _)) = best
+     in ((Just moveIndex, negate score), gen'')                     
